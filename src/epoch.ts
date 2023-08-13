00becb3388b7f8040c4ae4451c9d805f23f67fd2
Michael J. Roberts <84131395+michealroberts@users.noreply.github.com>
2023-08-13 15:59:38 +0100

    feat: Added getJulianDate() to epoch module in @observerly/astrometry.
    feat: Added getJulianDate() to epoch module in @observerly/astrometry. Includes associated test suite and expected API output.

--- src/epoch.ts
+++ src/epoch.ts
@@ -6,6 +6,24 @@
 
 /*****************************************************************************************************************/
 
-export {}
+import { utc } from './utc'
+
+/*****************************************************************************************************************/
+
+/**
+ *
+ * getJulianDate()
+ *
+ *
+ * @param date - The date for which to calculate the Julian Date (JD).
+ * @returns Julian Date as number - the Julian Date (JD) of the given date normalised to UTC.
+ *
+ */
+export const getJulianDate = (datetime: Date): number => {
+  const UTC = utc(datetime)
+
+  // Return the Julian Date (JD) of the given date normalised to UTC.
+  return UTC.getTime() / 86400000.0 + 2440587.5
+}
 
 /*****************************************************************************************************************/
---
+++ tests/epoch.spec.ts
@@ -0,0 +1,34 @@
+/*****************************************************************************************************************/
+
+// @author         Michael Roberts <michael@observerly.com>
+// @package        @observerly/astrometry/epoch
+// @license        Copyright © 2021-2023 observerly
+
+/*****************************************************************************************************************/
+
+import { describe, expect, it } from 'vitest'
+
+/*****************************************************************************************************************/
+
+import { getJulianDate } from '../src'
+
+/*****************************************************************************************************************/
+
+// For testing we need to specify a date because most calculations are
+// differential w.r.t a time component. We set it to the author's birthday:
+export const datetime = new Date('2021-05-14T00:00:00.000+00:00')
+
+/*****************************************************************************************************************/
+
+describe('getJulianDate', () => {
+  it('should be defined', () => {
+    expect(getJulianDate).toBeDefined()
+  })
+
+  it('should return the Julian Date (JD) of the given date', () => {
+    const JD = getJulianDate(datetime)
+    expect(JD).toBe(2459348.5)
+  })
+})
+
+/*****************************************************************************************************************/
