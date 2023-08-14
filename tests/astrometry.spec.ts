bb2fa913fe8a3d1cb819dd52591e4bac2326a916
Michael J. Roberts <84131395+michealroberts@users.noreply.github.com>
2023-08-14 11:12:00 +0100

    feat: Added getLocalSiderealTime() to astrometry module in @observerly/astrometry.
    feat: Added getLocalSiderealTime() to astrometry module in @observerly/astrometry. Includes associated test suite and expected API output.

--- tests/astrometry.spec.ts
+++ tests/astrometry.spec.ts
@@ -10,13 +10,16 @@ import { describe, expect, it } from 'vitest'
 
 /*****************************************************************************************************************/
 
-import { getGreenwhichSiderealTime } from '../src'
+import { getGreenwhichSiderealTime, getLocalSiderealTime } from '../src'
 
 /*****************************************************************************************************************/
 
 // For testing we need to specify a date because most calculations are
 // differential w.r.t a time component. We set it to the author's birthday:
 export const datetime = new Date('2021-05-14T00:00:00.000+00:00')
+
+// For testing we will fix the longitude to be Manua Kea, Hawaii, US:
+export const longitude = -155.468094
 
 /*****************************************************************************************************************/
 
@@ -25,10 +28,27 @@ describe('getGreenwhichSiderealTime', () => {
     expect(getGreenwhichSiderealTime).toBeDefined()
   })
 
-  it('should return the Julian Date (JD) of the given date', () => {
+  it('should return the Greenwhich Sidereal Time (GST) of the given date', () => {
     const GST = getGreenwhichSiderealTime(datetime)
     expect(GST).toBe(15.463990399019053)
   })
 })
 
 /*****************************************************************************************************************/
+
+describe('getLocalSiderealTime', () => {
+  it('should be defined', () => {
+    expect(getLocalSiderealTime).toBeDefined()
+  })
+
+  it('should return the Local Sidereal Time (LST) of the given date at longitude 0 at Greenwhich', () => {
+    const LST = getLocalSiderealTime(datetime, 0)
+    const GST = getGreenwhichSiderealTime(datetime)
+    expect(LST).toBe(GST)
+  })
+
+  it('should return the Local Sidereal Time (LST) of the given date', () => {
+    const LST = getLocalSiderealTime(datetime, longitude)
+    expect(LST).toBe(5.099450799019053)
+  })
+})
