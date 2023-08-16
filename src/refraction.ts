594a598383e01ad8fd1d7dba52d84a2d8ee36e95
Michael J. Roberts <84131395+michealroberts@users.noreply.github.com>
2023-08-16 13:46:47 +0100

    feat: Added getCorrectionToHorizontalForRefraction() to refraction module in @observerly/astrometry.
    feat: Added getCorrectionToHorizontalForRefraction() to refraction module in @observerly/astrometry. Includes associated test suite and expected API output.

--- src/refraction.ts
+++ src/refraction.ts
@@ -6,6 +6,50 @@
 
 /*****************************************************************************************************************/
 
-export {}
+import { type HorizontalCoordinate } from './common'
+
+import { convertDegreesToRadians as radians } from './utilities'
+
+/*****************************************************************************************************************/
+
+/**
+ *
+ * getCorrectionToHorizontalForRefraction()
+ *
+ * The eccentricity of the Earth's orbit is a measure of how elliptical the Earth's
+ * orbit is. The eccentricity of the Earth's orbit is not constant, but varies over
+ * time. This function returns the eccentricity of the Earth's orbit for a given date.
+ *
+ * @param target - The equatorial coordinate of the observed object.
+ * @param temperature - The temperature in Kelvin.
+ * @param pressure - The pressure in Pascals.
+ * @returns The equatorial coordinate of the observed object corrected for atmospheric refraction.
+ *
+ */
+export const getCorrectionToHorizontalForRefraction = (
+  target: HorizontalCoordinate,
+  temperature: number = 283.15,
+  pressure: number = 101325
+): HorizontalCoordinate => {
+  const { alt, az } = target
+
+  if (alt < 0) {
+    return target
+  }
+
+  // The pressure, in Pascals:
+  const P = pressure
+
+  // The temperature, in Kelvin:
+  const T = temperature
+
+  // Get the atmospheric refraction in degrees, corrected for temperature and pressure:
+  const R = (1.02 / Math.tan(radians(alt + 10.3 / (alt + 5.11))) / 60) * (P / 101325) * (283.15 / T)
+
+  return {
+    alt: alt + R,
+    az: az
+  }
+}
 
 /*****************************************************************************************************************/
---
+++ tests/refraction.spec.ts
@@ -0,0 +1,61 @@
+/*****************************************************************************************************************/
+
+// @author         Michael Roberts <michael@observerly.com>
+// @package        @observerly/astrometry/epoch
+// @license        Copyright © 2021-2023 observerly
+
+/*****************************************************************************************************************/
+
+import { describe, expect, it } from 'vitest'
+
+/*****************************************************************************************************************/
+
+import {
+  type EquatorialCoordinate,
+  convertEquatorialToHorizontal,
+  getCorrectionToHorizontalForRefraction
+} from '../src'
+
+/*****************************************************************************************************************/
+
+// For testing we need to specify a date because most calculations are
+// differential w.r.t a time component. We set it to the author's birthday:
+export const datetime = new Date('2021-05-14T00:00:00.000+00:00')
+
+// For testing we will fix the latitude to be Manua Kea, Hawaii, US
+export const latitude = 19.820611
+
+// For testing we will fix the longitude to be Manua Kea, Hawaii, US:
+export const longitude = -155.468094
+
+// For testing
+const betelgeuse: EquatorialCoordinate = { ra: 88.7929583, dec: 7.4070639 }
+
+/*****************************************************************************************************************/
+
+describe('getCorrectionToHorizontalForRefractione', () => {
+  it('should be defined', () => {
+    expect(getCorrectionToHorizontalForRefraction).toBeDefined()
+  })
+
+  it('should return the correctly adjusted horizontal coordinate adjusted for atmospheric refraction', () => {
+    const target = convertEquatorialToHorizontal(
+      datetime,
+      {
+        latitude,
+        longitude
+      },
+      betelgeuse
+    )
+
+    expect(target.az).toBe(134.44877920325155)
+    expect(target.alt).toBe(72.78539444063765)
+
+    const { alt, az } = getCorrectionToHorizontalForRefraction(target)
+    expect(az).toBe(target.az)
+    expect(alt).toBeGreaterThanOrEqual(72.79061860032508)
+    expect(alt).toBeLessThanOrEqual(73.0)
+  })
+})
+
+/*****************************************************************************************************************/
