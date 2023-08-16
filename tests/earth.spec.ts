48370300c469940a9cba194905276c66bf67f8ce
Michael J. Roberts <84131395+michealroberts@users.noreply.github.com>
2023-08-16 11:23:10 +0100

    feat: Added getEccentricityOfOrbit() to earth module in @observerly/astrometry.
    feat: Added getEccentricityOfOrbit() to earth module in @observerly/astrometry. Includes associated test suite and expected API output.

---
+++ tests/earth.spec.ts
@@ -0,0 +1,32 @@
+/*****************************************************************************************************************/
+
+// @author         Michael Roberts <michael@observerly.com>
+// @package        @observerly/astrometry/epoch
+// @license        Copyright © 2021-2023 observerly
+
+/*****************************************************************************************************************/
+
+import { describe, expect, it } from 'vitest'
+
+import { getEccentricityOfOrbit } from '../src'
+
+/*****************************************************************************************************************/
+
+// For testing we need to specify a date because most calculations are
+// differential w.r.t a time component. We set it to the author's birthday:
+export const datetime = new Date('2021-05-14T00:00:00.000+00:00')
+
+/*****************************************************************************************************************/
+
+describe('getEccentricityOfOrbit', () => {
+  it('should be defined', () => {
+    expect(getEccentricityOfOrbit).toBeDefined()
+  })
+
+  it("should return the eccentricity of the Earth's orbit for the given datetime", () => {
+    const e = getEccentricityOfOrbit(datetime)
+    expect(e).toBe(0.016699647287906946)
+  })
+})
+
+/*****************************************************************************************************************/
