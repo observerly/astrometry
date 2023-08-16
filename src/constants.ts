b76f68a322e9f230865327b117fe32cb5cf6e985
Michael J. Roberts <84131395+michealroberts@users.noreply.github.com>
2023-08-16 14:32:15 +0100

    feat: Added J1900 to constants module in @observerly/astrometry.
    feat: Added J1900 to constants module in @observerly/astrometry.

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,15 @@
 
 /*****************************************************************************************************************/
 
-export {}
+/**
+ *
+ * The previous standard epoch "J1900" was defined by international agreement to
+ * be equivalent to: The Gregorian date January 0.5, 1900, at 12:00 TT (Terrestrial Time),
+ * equivalent to noon on December 31, 1899.
+ *
+ * The Julian date 2415020.0 TT (Terrestrial Time).
+ *
+ */
+export const J1900 = 2415020.0 as const
 
 /*****************************************************************************************************************/
