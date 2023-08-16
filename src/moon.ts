bd3fca6dc5e51bc8477d0167d0a47d4e1190581f
Michael J. Roberts <84131395+michealroberts@users.noreply.github.com>
2023-08-16 16:35:20 +0100

    feat: Added getMeanAnomaly() to moon module in @observerly/astrometry.
    feat: Added getMeanAnomaly() to moon module in @observerly/astrometry. Includes associated test suite and expected API output.

--- src/moon.ts
+++ src/moon.ts
@@ -6,6 +6,42 @@
 
 /*****************************************************************************************************************/
 
-export {}
+import { getJulianDate } from './epoch'
+
+/*****************************************************************************************************************/
+
+/**
+ *
+ * getMeanAnomaly()
+ *
+ * The Moon's mean anomaly is the angle between perigee and the Moon's current position.
+ *
+ * @param date - The date to calculate the Moon's mean anomaly for.
+ * @returns The Moon's mean anomaly at the given date.
+ *
+ */
+export const getMeanAnomaly = (datetime: Date): number => {
+  // Get the Julian date:
+  const JD = getJulianDate(datetime)
+
+  // Calculate the number of centuries since J2000.0:
+  const T = (JD - 2451545.0) / 36525
+
+  // Get the Moon's mean anomaly at the current epoch relative to J2000:
+  let M =
+    (134.9634114 +
+      477198.8676313 * T +
+      0.008997 * Math.pow(T, 2) +
+      Math.pow(T, 3) / 69699 -
+      Math.pow(T, 4) / 14712000) %
+    360
+
+  // Correct for negative angles
+  if (M < 0) {
+    M += 360
+  }
+
+  return M
+}
 
 /*****************************************************************************************************************/
