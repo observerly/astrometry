3e65218670b82063fe39a2b8495c5e192799a842
Michael J. Roberts <84131395+michealroberts@users.noreply.github.com>
2023-08-16 14:41:31 +0100

    feat: Added J2000 to constants module in @observerly/astrometry.
    feat: Added J2000 to constants module in @observerly/astrometry.

--- src/constants.ts
+++ src/constants.ts
@@ -34,3 +34,15 @@ export const J1900 = 2415020.0 as const
 export const J1970 = 2440587.5 as const
 
 /*****************************************************************************************************************/
+
+/**
+ *
+ * The currently-used standard epoch "J2000" is defined by international agreement to
+ * be equivalent to: The Gregorian date January 1, 2000, at 12:00 TT (Terrestrial Time).
+ *
+ * The Julian date 2451545.0 TT (Terrestrial Time).
+ *
+ */
+export const J2000 = 2451545.0 as const
+
+/*****************************************************************************************************************/
