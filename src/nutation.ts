5680b87fcfb953b1e8c7413ddc9ab698ff681510
Michael J. Roberts <84131395+michealroberts@users.noreply.github.com>
2023-08-22 14:53:27 +0100

    feat: Added nutation module to @observerly/astrometry.
    feat: Added nutation module to @observerly/astrometry.

--- src/nutation.ts
+++ src/nutation.ts
@@ -1,23 +1,11 @@
 /*****************************************************************************************************************/
 
 // @author         Michael Roberts <michael@observerly.com>
-// @package        @observerly/typescript-vite-template
+// @package        @observerly/astrometry/nutation
 // @license        Copyright © 2021-2023 observerly
 
 /*****************************************************************************************************************/
 
-export * from './abberation'
-export * from './astrometry'
-export * from './common'
-export * from './constants'
-export * from './coordinates'
-export * from './earth'
-export * from './epoch'
-export * from './moon'
-export * from './nutation'
-export * from './precession'
-export * from './refraction'
-export * from './seeing'
-export * from './sun'
+export {}
 
 /*****************************************************************************************************************/
