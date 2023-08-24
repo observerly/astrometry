951d9e6b376cf17043293383f916c6da31c048e0
Michael J. Roberts <84131395+michealroberts@users.noreply.github.com>
2023-08-24 16:45:57 +0100

    feat: Added observation module to @observerly/astrometry.
    feat: Added observation module to @observerly/astrometry.

--- src/observation.ts
+++ src/observation.ts
@@ -1,24 +1,11 @@
 /*****************************************************************************************************************/
 
 // @author         Michael Roberts <michael@observerly.com>
-// @package        @observerly/typescript-vite-template
+// @package        @observerly/astrometry/observation
 // @license        Copyright © 2021-2023 observerly
 
 /*****************************************************************************************************************/
 
-export * from './abberation'
-export * from './astrometry'
-export * from './common'
-export * from './constants'
-export * from './coordinates'
-export * from './earth'
-export * from './epoch'
-export * from './moon'
-export * from './nutation'
-export * from './observation'
-export * from './precession'
-export * from './refraction'
-export * from './seeing'
-export * from './sun'
+export {}
 
 /*****************************************************************************************************************/
