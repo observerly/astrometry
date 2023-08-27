96609b3e37364ca73a0ef879a5707d2e5cdbebc9
Michael J. Roberts <84131395+michealroberts@users.noreply.github.com>
2023-08-27 14:17:19 +0100

    feat: Added isBodyCircumpolar() to transit module in @observerly/astrometry.
    feat: Added isBodyCircumpolar() to transit module in @observerly/astrometry. Includes associated test suite and expected API output.

--- src/transit.ts
+++ src/transit.ts
@@ -1,11 +1,44 @@
 /*****************************************************************************************************************/
 
 // @author         Michael Roberts <michael@observerly.com>
-// @package        @observerly/astrometry/transits
+// @package        @observerly/astrometry/transit
 // @license        Copyright © 2021-2023 observerly
 
 /*****************************************************************************************************************/
 
-export {}
+import { type EquatorialCoordinate, type GeographicCoordinate } from './common'
+
+/*****************************************************************************************************************/
+
+/**
+ *
+ * isBodyCircumpolar()
+ *
+ * An object is considered circumpolar if it is always above the observer's horizon
+ * and never sets. This is true when the object's declination is greater than 90
+ * degrees minus the observer's latitude.
+ *
+ * @param observer - The geographic coordinate of the observer.
+ * @param target - The equatorial coordinate of the observed object.
+ * @param horizon - The observer's horizon (in degrees).
+ * @returns a boolean indicating whether the target is circumpolar.
+ */
+export const isBodyCircumpolar = (
+  observer: GeographicCoordinate,
+  target: EquatorialCoordinate,
+  horizon: number = 0
+): boolean => {
+  // We only need to consider the latitude of the observer:
+  const { latitude } = observer
+
+  // We only need to consider the declination of the target object:
+  const { dec } = target
+
+  const extrema = 90 - Math.abs(latitude) - horizon
+
+  // If the object's declination is greater than 90 degrees minus the observer's latitude,
+  // then the object is circumpolar (always above the observer's horizon and never sets).
+  return latitude >= 0 ? dec > extrema : dec < extrema
+}
 
 /*****************************************************************************************************************/
