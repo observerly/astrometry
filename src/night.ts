52ce4e44e1e46d346bdd505344c2998fb40b0b5e
Michael J. Roberts <84131395+michealroberts@users.noreply.github.com>
2023-09-03 15:55:15 +0100

    feat: Added getSolarTransit() to sun module in @observerly/astrometry.
    feat: Added getSolarTransit() to sun module in @observerly/astrometry. Includes associated test suite and expected API output.

--- src/night.ts
+++ src/night.ts
@@ -6,6 +6,85 @@
 
 /*****************************************************************************************************************/
 
-export {}
+import { type GeographicCoordinate, type HorizontalCoordinate } from './common'
+
+import { convertEquatorialToHorizontal } from './coordinates'
+
+import { getCorrectionToHorizontalForRefraction } from './refraction'
+
+import { getSolarEquatorialCoordinate } from './sun'
+
+/*****************************************************************************************************************/
+
+export const getSolarTransit = (
+  datetime: Date,
+  observer: GeographicCoordinate,
+  horizon: number = -12,
+  temperature: number = 288.15,
+  pressure: number = 101325
+): {
+  sunrise: Date | null
+  noon: Date | null
+  sunset: Date | null
+} => {
+  // Set the datetime to be at 1 minute before midnight for the previous date:
+  datetime = new Date(new Date(datetime.setHours(0, 0, 0, 0)).getTime())
+
+  const sun: (HorizontalCoordinate & { datetime: Date })[] = []
+
+  let rise: number | null = null
+
+  let noon: Date | null = null
+
+  let set: number | null = null
+
+  for (let i = 0; i < 1440; i++) {
+    const eq = getSolarEquatorialCoordinate(datetime)
+
+    const target = convertEquatorialToHorizontal(datetime, observer, eq)
+
+    // Correct the altitude for refraction:
+    const refraction = getCorrectionToHorizontalForRefraction(target, temperature, pressure)
+
+    // Find the altitude where the sun passes above the horizon:
+    if (refraction.alt > horizon && rise === null) {
+      rise = i
+    }
+
+    // Find the altitude where the sun passes below the horizon:
+    if (refraction.alt < horizon && rise !== null && set === null) {
+      set = i
+    }
+
+    // If the altitude is above the horizon, add it to the list of altitudes:
+    if (refraction.alt > horizon) {
+      sun.push({
+        alt: refraction.alt,
+        az: refraction.az,
+        datetime: new Date(datetime.getTime())
+      })
+    }
+
+    // Add one minute to the datetime:
+    datetime = new Date(datetime.setMinutes(datetime.getMinutes() + 1))
+  }
+
+  // Set the datetime to be at 1 minute before midnight for the previous date:
+  datetime = new Date(datetime.getTime() - 60000 * 1440)
+
+  // Find the index of the maximum altitude from the list of altitudes:
+  const transit = sun.findIndex(s => s.alt === Math.max(...sun.map(s => s.alt)))
+
+  // Get the time of the maximum altitude:
+  noon = sun[transit].datetime
+
+  // Get the time of the sunrise:
+  const sunrise = rise !== null ? new Date(datetime.getTime() + 60000 * rise) : null
+
+  // Get the time of the sunset:
+  const sunset = set !== null ? new Date(datetime.getTime() + 60000 * set) : null
+
+  return { sunrise, noon, sunset }
+}
 
 /*****************************************************************************************************************/
---
+++ tests/night.spec.ts
@@ -0,0 +1,69 @@
+/*****************************************************************************************************************/
+
+// @author         Michael Roberts <michael@observerly.com>
+// @package        @observerly/astrometry/moon
+// @license        Copyright © 2021-2023 observerly
+
+/*****************************************************************************************************************/
+
+import { describe, expect, it } from 'vitest'
+
+/*****************************************************************************************************************/
+
+import { getSolarTransit } from '../src'
+
+/*****************************************************************************************************************/
+
+// For testing we need to specify a date because most calculations are
+// differential w.r.t a time component. We set it to the author's birthday:
+export const datetime = new Date('2021-05-14T03:00:00.000+00:00')
+
+// For testing, we will fix the latitude to be the Isles of Scilly, Cornwall, UK.
+export const latitude: number = 49.914425
+
+// For testing, we will fix the longitude to be the Isles of Scilly, Cornwall, UK.
+export const longitude: number = -6.315165
+
+/*****************************************************************************************************************/
+
+describe('getSolarTransit', () => {
+  it('should return the correct solar transit for the observer at a horizon of 0 degrees', () => {
+    const { sunrise, noon, sunset } = getSolarTransit(
+      datetime,
+      {
+        latitude,
+        longitude
+      },
+      0
+    )
+
+    expect(sunrise).toBeInstanceOf(Date)
+    expect(noon).toBeInstanceOf(Date)
+    expect(sunset).toBeInstanceOf(Date)
+
+    expect(sunrise?.toISOString()).toBe('2021-05-14T04:46:00.000Z')
+    expect(noon?.toISOString()).toBe('2021-05-14T12:20:00.000Z')
+    expect(sunset?.toISOString()).toBe('2021-05-14T19:56:00.000Z')
+  })
+
+  it('should return the correct solar transit for the observer at a horizon of -6 degrees', () => {
+    const { sunrise, noon, sunset } = getSolarTransit(
+      datetime,
+      {
+        latitude,
+        longitude
+      },
+      -6
+    )
+
+    expect(sunrise).toBeInstanceOf(Date)
+    expect(noon).toBeInstanceOf(Date)
+    expect(sunset).toBeInstanceOf(Date)
+
+    expect(sunrise?.toISOString()).toBe('2021-05-14T04:01:00.000Z')
+    expect(noon?.toISOString()).toBe('2021-05-14T12:20:00.000Z')
+    expect(sunset?.toISOString()).toBe('2021-05-14T20:41:00.000Z')
+  })
+})
+
+/*****************************************************************************************************************/
