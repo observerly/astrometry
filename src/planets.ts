4a0020c2293f2a25054b14c3df54f631edff9082
Michael J. Roberts <84131395+michealroberts@users.noreply.github.com>
2023-10-19 16:56:21 +0100

    feat: Added Planet type to planets module in @observerly/astrometry.
    feat: Added Planet type to planets module in @observerly/astrometry.

--- src/planets.ts
+++ src/planets.ts
@@ -6,6 +6,98 @@
 
 /*****************************************************************************************************************/
 
-export {}
+export type Planet = {
+  /**
+   *
+   *
+   * Some unique identifier for the planet.
+   *
+   */
+  uid: string
+  /**
+   *
+   *
+   * The common, wsternised name of the planet.
+   *
+   */
+  name: 'Mercury' | 'Venus' | 'Earth' | 'Mars' | 'Jupiter' | 'Saturn' | 'Uranus' | 'Neptune'
+  /**
+   *
+   *
+   * The "astrological" symbol for the planet.
+   *
+   */
+  symbol: '☿' | '♀' | '♁' | '♂' | '♃' | '♄' | '♅' | '♆'
+  /**
+   *
+   *
+   * The planet's orbital period in tropical years.
+   *
+   */
+  T: number
+  /**
+   *
+   *
+   * The planet's mass relative to Earth.
+   *
+   */
+  m: number
+  /**
+   *
+   *
+   * The planet's radius relative to Earth.
+   *
+   */
+  r: number
+  /**
+   *
+   *
+   * The planet's eccentricity.
+   *
+   */
+  e: number
+  /**
+   *
+   *
+   * The planet's semi-major axis in AU.
+   *
+   */
+  a: number
+  /**
+   *
+   *
+   * The planet's inclination in degrees.
+   *
+   */
+  i: number
+  /**
+   *
+   *
+   * The planet's ecliptical longitude at epoch in degrees.
+   *
+   */
+  ε: number
+  /**
+   *
+   *
+   * The planet's ecliptical longitude at perihelion in degrees.
+   *
+   */
+  ϖ: number
+  /**
+   *
+   *
+   * The planet's ecliptical longitude of ascending node at epoch in degrees.
+   *
+   */
+  Ω: number
+  /**
+   *
+   *
+   * Whether the planet is inferior.
+   *
+   */
+  isInferior?: boolean
+}
 
 /*****************************************************************************************************************/
