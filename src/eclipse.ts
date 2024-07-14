8233e52690eca48bf614a36316a19352687481bc
Michael J. Roberts <michael@observerly.com>
2024-07-14 19:22:04 +0100

    feat: add EclipseType enum to @observerly/astrometry
    feat: add EclipseType enum to @observerly/astrometry

--- src/eclipse.ts
+++ src/eclipse.ts
@@ -6,6 +6,49 @@
 
 /*****************************************************************************************************************/
 
-export {}
+/**
+ * The different types of lunar and solar eclipses.
+ *
+ * @readonly
+ * @enum {string}
+ *
+ */
+export enum EclipseType {
+  /**
+   *
+   * @member {string} Penumbral
+   *
+   * A lunar eclipse in which only the Earth's penumbra falls on the Moon.
+   *
+   */
+  Penumbral = 'penumbral',
+  /**
+   *
+   * @member {string} Partial
+   *
+   * A partial lunar/solar eclipse.
+   *
+   */
+  Partial = 'partial',
+  /**
+   *
+   * @member {string} Annular
+   *
+   * A solar eclipse in which the entire Moon is visible against the Sun,
+   * but the Sun appears as a ring around the Moon.
+   *
+   * N.B. Never used for a lunar eclipse.
+   *
+   */
+  Annular = 'annular',
+  /**
+   *
+   * @member {string} Total
+   *
+   * A total lunar/solar eclipse.
+   *
+   */
+  Total = 'total'
+}
 
 /*****************************************************************************************************************/
