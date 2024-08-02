cba3034bb0ccca5cc837f6ebf55194355f1900c0
michealroberts <michael@observerly.com>
2024-08-02 17:33:07 +0100

    refactor: amend q() utility to calculate Lunar altitude in @observerly/astrometry
    refactor: amend q() utility to calculate Lunar altitude in @observerly/astrometry

--- src/q.ts
+++ src/q.ts
@@ -10,7 +10,11 @@ import { getCorrectionToEquatorialForAbberation } from './abberation'
 
 import { getAngularSeparation } from './astrometry'
 
-import { type EquatorialCoordinate, type GeographicCoordinate } from './common'
+import {
+  HorizontalCoordinate,
+  type EquatorialCoordinate,
+  type GeographicCoordinate
+} from './common'
 
 import { convertEquatorialToHorizontal } from './coordinates'
 
@@ -71,7 +75,13 @@ type Q = EquatorialCoordinate & {
 // The Q index is a measure of the quality of the an observation, which takes into account the
 // illumination of the Moon, the altitude of the target, and the altitude of the Sun as well as
 // the angular separation between the Moon and the target.
-export const q = (K: number, φ: number, A: number, alt: number): number => {
+export const q = (
+  K: number,
+  φ: number,
+  A: number,
+  sun: HorizontalCoordinate,
+  moon: HorizontalCoordinate
+): number => {
   // The Moon's illumination factor, which has a range of 0 to 100 percent:
   const k = 1 - 2 * (K / 100)
 
@@ -82,7 +92,12 @@ export const q = (K: number, φ: number, A: number, alt: number): number => {
   // Moon Q, which takes into account the Moon's illumination and the distance from
   // the Moon to the target (e.g., we can discount the Lunar illumination factor if
   // the distance is greater than 60 degrees):
-  const MQ = φ >= 60 ? a : K < 25 ? a : -(a * k)
+  let MQ = φ >= 60 ? a : K < 25 ? a : -(a * k)
+
+  // If the Moon is below the horizon, then the Moon Q is 1:
+  if (moon.alt <= 0) {
+    MQ = 1
+  }
 
   // Target Q, which takes into account the altitude of the target:
   // We accept that a value greater than 6 degrees is a good altitude for the target:
@@ -90,7 +105,7 @@ export const q = (K: number, φ: number, A: number, alt: number): number => {
 
   // Sun Q, which takes into account the altitude of the Sun:
   // We accept that a value of less than -18 degrees is a good altitude for the Sun:
-  const SQ = alt <= -18 ? -alt / 72 - 1 / 4 : -alt / 108 - 1 / 6
+  const SQ = sun.alt <= -18 ? -sun.alt / 72 - 1 / 4 : -sun.alt / 108 - 1 / 6
 
   // The Q index is the average of the Moon Q, Target Q, and Sun Q, which has a range of -1 to 1:
   return (MQ + TQ + SQ) / 3
@@ -169,7 +184,7 @@ export function getQIndex(
   return {
     ra,
     dec,
-    Q: q(K, φ, A, sun.alt),
+    Q: q(K, φ, A, sun, moon),
     K,
     φ,
     A,
