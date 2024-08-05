4183011fc1368158628e90a9ddc83e55272bee42
michealroberts <michael@observerly.com>
2024-08-05 15:22:10 +0100

    feat: add Lupus constellation module in @observerly/astrometry
    feat: add Lupus constellation module in @observerly/astrometry

--- src/constellations.ts
+++ src/constellations.ts
@@ -63,6 +63,7 @@ export { leo } from './constellations/leo'
 export { leoMinor } from './constellations/leoMinor'
 export { lepus } from './constellations/lepus'
 export { libra } from './constellations/libra'
+export { lupus } from './constellations/lupus'
 
 /*****************************************************************************************************************/
 
