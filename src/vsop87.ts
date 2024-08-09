e7db263b134e951ff9668d373c06c5ce87208e91
michealroberts <michael@observerly.com>
2024-08-09 12:31:12 +0100

    feat: add calculateR to vsop87 module in @observerly/astrometry
    feat: add calculateR to vsop87 module in @observerly/astrometry

--- src/vsop87.ts
+++ src/vsop87.ts
@@ -6,6 +6,8 @@
 
 /*****************************************************************************************************************/
 
+import { AU_IN_METERS } from './constants'
+
 import { convertRadiansToDegrees as degrees } from './utilities'
 
 /*****************************************************************************************************************/
@@ -40,6 +42,20 @@ export function calculateB(τ: number, terms: { A: number; B: number; C: number 
 
   // The result must be converted from 1e-8 radians to radians and then to degrees:
   return degrees(B / 1e8)
+}
+
+/*****************************************************************************************************************/
+
+export function calculateR(τ: number, terms: { A: number; B: number; C: number }[][]): number {
+  let R = 0
+
+  for (let i = 0; i < terms.length; i++) {
+    const r = calculateTerms(τ, terms[i])
+    R += r * τ ** i
+  }
+
+  // The result must be converted from 1e-8 AU to AU and then to meters:
+  return (R / 1e8) * AU_IN_METERS
 }
 
 /*****************************************************************************************************************/
