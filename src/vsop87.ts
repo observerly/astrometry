2f2028a62119bc87a14cf32375dffbfe8cdd2f21
michealroberts <michael@observerly.com>
2024-08-09 12:25:24 +0100

    feat: add calculateL to vsop87 module in @observerly/astrometry
    feat: add calculateL to vsop87 module in @observerly/astrometry

--- src/vsop87.ts
+++ src/vsop87.ts
@@ -6,8 +6,26 @@
 
 /*****************************************************************************************************************/
 
+import { convertRadiansToDegrees as degrees } from './utilities'
+
+/*****************************************************************************************************************/
+
 function calculateTerms(τ: number, terms: { A: number; B: number; C: number }[]): number {
   return terms.reduce((sum, term) => sum + term.A * Math.cos(term.B + term.C * τ), 0)
+}
+
+/*****************************************************************************************************************/
+
+export function calculateL(τ: number, terms: { A: number; B: number; C: number }[][]): number {
+  let L = 0
+
+  for (let i = 0; i < terms.length; i++) {
+    const l = calculateTerms(τ, terms[i])
+    L += l * τ ** i
+  }
+
+  // The result must be converted from 1e-8 radians to radians and then to degrees:
+  return degrees(L / 1e8)
 }
 
 /*****************************************************************************************************************/
